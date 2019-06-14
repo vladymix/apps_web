98b1449f5fe013462c9763acc0eaa779f81b6a9c
Fabricio <fabricio.altamirano@exaccta.com>
2019-06-14 14:20:54 +0200

    create privacity for mis gastos

---
+++ vendor/data_privacity.js
@@ -0,0 +1,41 @@
+var emtHorarios = [
+    {
+        "title": "Acerca de la privacidad y Localización en la aplicación",
+        "description": "Obtén información acerca de los servicios que te ofrece EMT Horarios y como manejamos la información de tu ubicación y tu perfil público para una publicidad controlada"
+    },
+    {
+        "title": "¿Que datos recopilan?",
+        "description": "La aplicación EMT Horarios accede a su ubicación solo para hacer el seguimiento de su posición para el seguimiento de las lineas de autobús o para buscar las paradas cercanas con un radio de 300m, en ningún caso comparte su ubicación con terceros u otras personas, en todo caso esta puede ser desactivada en el apartado de configuración de la aplicación."
+    },
+    {
+        "title": "Datos de uso",
+        "description": "La aplicación usa servicios de terceros como firebase de google para hacer un seguimiento de la actividad de la aplicación para más información vease: <a href='https://firebase.google.com/support/privacy?hl=es-419'>Política firebase</a>."
+    }
+    , {
+        "title": "Activar o desactivar Localización",
+        "description": "Puedes activar o desactivar el acceso a tu ubicación en <span> Ajustes > Aplicaciones > EMT Horarios > Permisos </span>Aqui puedes controlar individualmente los permisos para cada aplicación"
+    }
+    , {
+        "title": "Publicidad",
+        "description": "Esta aplicación hace uso de proveedores de publicidad y gracias a ellos la aplicación puede seguir mejorando si quieres saber más dirigete a <a href='https://support.google.com/admob/answer/6128543?hl=es'>Política AdMon</a> "
+    },
+    {
+        "title": "Adquisición de datos",
+        "description": "Los datos proporcionados de tiempos de espera, ruta de lineas de autobús y planos esquemáticos son proporcionados gracias a <a href='https://opendata.emtmadrid.es/Home'>EMT Madrid</a>"
+    }
+];
+
+
+var expenses = [
+    {
+        "title": "Recopilación y uso de información",
+        "description": "Al igual que la mayoria de aplicaciónes cada vez que ejecutas nuestra aplicacion recopilamos datos sobre el uso y los errores que se produzcan al utilizarla"
+    },
+    {
+        "title": "¿Que datos recopilan?",
+        "description": "La aplicación usa servicios de terceros como firebase de google para hacer un seguimiento de la actividad de la aplicación para más información vease: <a href='https://firebase.google.com/support/privacy?hl=es-419'>Política firebase</a>."
+    }, {
+        "title": "Publicidad",
+        "description": "Esta aplicación hace uso de proveedores de publicidad y gracias a ellos la aplicación puede seguir mejorando si quieres saber más dirigete a <a href='https://support.google.com/admob/answer/6128543?hl=es'>Política AdMon</a> "
+    }
+];
